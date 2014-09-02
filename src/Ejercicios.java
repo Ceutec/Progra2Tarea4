a49cc528832dcdb8a7990a72f08a93c522e25a0f
Turupawn <turupawn@userr>
2014-09-02 15:48:16 -0600

    primer commit

---
+++ src/Ejercicios.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+class MiClase
+{
+	int x;
+	public MiClase(int x) {
+		this.x = x;
+	}
+};
+
+public class Ejercicios {
+
+	//Devuelve la division entre 'a' y 'b'
+	//Se deben manejar las excepciones en caso de dividir entre 0
+	static int dividir(int a, int b)
+	{
+		return -1;
+	}
+	
+	//Devuelve el valor almacenado en 'arreglo' en la posicion 'pos'
+	//Se deben manejar las excepciones en caso la posicion se salga de los limites
+	static int getNumero(int arreglo[], int pos)
+	{
+		return -1;
+	}
+	
+	//Devuelve la variable 'x' de 'mi_clase'
+	//Se debe manejar las excepciones en caso que 'mi_clase' sea igual a null
+	static int getX(MiClase mi_clase)
+	{
+		return -1;
+	}
+	
+	//Devuelve el promedio de todos los elementos de 'mi_lista'
+	static int getPromedio(ArrayList<Integer> mi_lista)
+	{
+		return -1;
+	}
+	
+	//Devuelve el numero mayor almacenado en 'mi_lista'
+	static int getMayor(ArrayList<Integer> mi_lista)
+	{
+		return -1;
+	}
+	
+	//Devuelve true si 'mi_lista' contiene 'str' de lo contrario devuelve false
+	static boolean existe(ArrayList<String> mi_lista, String str)
+	{
+		return false;
+	}
+	
+	public static void main(String[] args)
+	{ 
+	}
+
+}
